b83f61fd44afad572e83541271dcd65657384c6c
PritamShakyawal <95740918+PritamShakyawal@users.noreply.github.com>
2024-07-03 20:57:44 +0530

    Time: 393 ms (96.58%), Space: 0B (100.00%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
 # select name from customer where referee_id != 2 or referee_id is null;
 
-select name from customer where IFNULL(referee_id,0) != 2;
+# select name from customer where IFNULL(referee_id,0) != 2;
+
+select name from customer where id Not in (
+    select id from customer where referee_id = 2
+)
