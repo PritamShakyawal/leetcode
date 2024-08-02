b3277486180b33d3311b79a944bbb9d81c30abce
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-02 22:44:52 +0530

    Time: 13 ms (25.98%), Space: 8.6 MB (61.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <string>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::sort(s.begin(), s.end());
+    std::sort(t.begin(), t.end());
+        
+    return s==t;
+    }
+};
