7c5e4537e894e04fbcdf2a7c5782c8941f5ed20b
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-13 01:14:15 +0530

    Time: 4 ms (89.98%), Space: 15.7 MB (39.11%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,60 +1,60 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        // stack <int> scores;
-        // for(auto&op : tokens){
-        //      if (op == "+") {
-        //     int top = scores.top(); scores.pop();
-        //     int newTop = scores.top(); scores.pop();
-        //     scores.push(newTop + top);
-        //     }else if (op == "-"){
-        //         int top = scores.top();
-        //      scores.pop();
-        //      int newTop = scores.top();
-        //      // scores.push(top);
-        //      scores.push(newTop - top);
-        //     } else if (op == "*") {
-        //     int top = scores.top(); scores.pop();
-        //     int newTop = scores.top(); scores.pop();
-        //     scores.push(newTop * top);
-        //      }
-        //     else if (op == "/") {
-        //     int top = scores.top(); scores.pop();
-        //     int newTop = scores.top(); scores.pop();
-        //     scores.push(newTop / top);
-        //     }
-        //     else{
-        //         scores.push(stoi(op));
-        //     }
-        // }
-        // return scores.top();
-        
-        
-        stack<int> scores;
-
-    for (const auto& op : tokens) {
-        if (op == "+") {
+        stack <int> scores;
+        for(auto&op : tokens){
+             if (op == "+") {
             int top = scores.top(); scores.pop();
             int newTop = scores.top(); scores.pop();
             scores.push(newTop + top);
-        } else if (op == "-") {
-            int top = scores.top(); scores.pop();
-            int newTop = scores.top(); scores.pop();
-            scores.push(newTop - top);
-        } else if (op == "*") {
+            }else if (op == "-"){
+                int top = scores.top();
+             scores.pop();
+             int newTop = scores.top();
+             scores.pop();
+             scores.push(newTop - top);
+            } else if (op == "*") {
             int top = scores.top(); scores.pop();
             int newTop = scores.top(); scores.pop();
             scores.push(newTop * top);
-        } else if (op == "/") {
+             }
+            else if (op == "/") {
             int top = scores.top(); scores.pop();
             int newTop = scores.top(); scores.pop();
-            // Integer division should truncate towards zero
             scores.push(newTop / top);
-        } else {
-            scores.push(std::stoi(op));
+            }
+            else{
+                scores.push(stoi(op));
+            }
         }
-    }
+        return scores.top();
+        
+        
+//         stack<int> scores;
 
-    return scores.top();
+//     for (const auto& op : tokens) {
+//         if (op == "+") {
+//             int top = scores.top(); scores.pop();
+//             int newTop = scores.top(); scores.pop();
+//             scores.push(newTop + top);
+//         } else if (op == "-") {
+//             int top = scores.top(); scores.pop();
+//             int newTop = scores.top(); scores.pop();
+//             scores.push(newTop - top);
+//         } else if (op == "*") {
+//             int top = scores.top(); scores.pop();
+//             int newTop = scores.top(); scores.pop();
+//             scores.push(newTop * top);
+//         } else if (op == "/") {
+//             int top = scores.top(); scores.pop();
+//             int newTop = scores.top(); scores.pop();
+//             // Integer division should truncate towards zero
+//             scores.push(newTop / top);
+//         } else {
+//             scores.push(stoi(op));
+//         }
+//     }
+
+//     return scores.top();
     }
 };
