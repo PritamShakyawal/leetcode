40a66250d4000d9369639d1333782ec50e310e19
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-17 21:24:31 +0530

    Time: 4 ms (31.70%), Space: 9.5 MB (30.23%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -19,9 +19,27 @@ class Solution {
     }
 public:
     int rob(vector<int>& nums) {
+        // int n = nums.size();
+        // vector<int> dp(n+1, -1);
+        // int ans = f(nums, n-1, dp);
+        // return ans;
+        
+        
+        
         int n = nums.size();
         vector<int> dp(n+1, -1);
-        int ans = f(nums, n-1, dp);
-        return ans;
+        dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+        
+        for(int i=1; i<n; i++){
+        int pick = nums[i];  if(i>1) {
+            pick += dp[i-2];
+        }
+        int notPick = dp[i-1];
+            
+            dp[i] = max(pick, notPick); 
+        }
+        return dp[n - 1];
+        
     }
 };
