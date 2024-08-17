6e3bbdb4c24b12e3ef9e2a56f9ad8f5def539a5a
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-17 12:46:55 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (46.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -18,12 +18,23 @@ public:
 //         dp[n] = climbStairs(n-1) + climbStairs(n-2);
 //         return dp[n];
         
-        vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        dp[1] = 1;
+        // vector<int> dp(n+1, -1);
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i=2; i<=n; i++) {
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+        
+        
+        int prev1 = 1;
+        int prev2 = 1;
+        int cur=1;
         for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
         }
-        return dp[n];
+        return cur;
     }
 };
