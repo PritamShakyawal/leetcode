43122b85c15c5ca0a14e9cf8474ae9a27dbfdc7a
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-25 11:55:43 +0530

    Time: 28 ms (18.42%), Space: 14.7 MB (24.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,7 +4,7 @@ public:
         if(s.size() == 0)
          return 0;
   int maxans = INT_MIN;
-  unordered_set < int > set;
+  unordered_set < char > set;
   int l = 0;
   for (int r = 0; r < s.length(); r++)
   {
@@ -19,5 +19,47 @@ public:
     maxans = max(maxans, r - l + 1);
   }
   return maxans;
+        
+        
+        
+        
+        
+        
+//       vector < int > mpp(256, -1);
+//       int left = 0, right = 0;
+//       int n = s.size();
+//       int len = 0;
+//       while (right < n) {
+//         if (mpp[s[right]] != -1)
+//           left = max(mpp[s[right]] + 1, left);
+
+//         mpp[s[right]] = right;
+
+//         len = max(len, right - left + 1);
+//         right++;
+//       }
+//       return len;
+        
+        
+        
+        
+//     if(s.size()==0)
+//       return 0;
+//   int maxans = INT_MIN;
+//   for (int i = 0; i < s.length(); i++)
+//   {
+//     unordered_set < int > set;
+//     for (int j = i; j < s.length(); j++) 
+//     {
+//       if (set.find(s[j]) != set.end())
+//       {
+//         maxans = max(maxans, j - i);
+//         break;
+//       }
+        
+//       set.insert(s[j]);
+//     }
+//   }
+//   return maxans;
     }
 };
