a5bb21452c7c4592402d85bb915c8da2c6704ee8
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-25 11:38:42 +0530

    Time: 85 ms (58.90%), Space: 96.2 MB (58.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
     int n = prices.size();
     int p = prices[0];
     int maxiPro = 0;
-    for(int i=0; i<n; i++){
+    for(int i=1; i<n; i++){
             int cost = prices[i] - p;
             maxiPro = max(cost, maxiPro);
             p = min(p, prices[i]);
