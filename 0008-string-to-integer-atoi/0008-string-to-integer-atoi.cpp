34367b4b808597908ea366782e82b5e63b313c2a
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-25 15:05:31 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (64.53%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int  num = 0;
+        int sign = 1;
+        int i = 0;
+        int intMax = INT_MAX;
+        int intMin = INT_MIN;
+        
+        while(i<s.length() && s[i] == ' '){
+            i++;
+        }
+        if(i<s.length() && (s[i]=='-' || s[i]=='+')){
+            sign = (s[i]=='-') ? -1 : 1;
+            i++;
+        }
+        while(i<s.size() && isdigit(s[i])){
+            int digit = s[i] - '0';
+        
+        // Check if num * 10 + digit would cause overflow
+        if (num > (intMax - digit) / 10) {
+            return (sign == 1) ? intMax : intMin;
+        }
+
+        num = num * 10 + digit;
+        i++;
+        }
+        
+        return num*sign;
+    }
+};
