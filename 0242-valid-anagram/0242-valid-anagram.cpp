3e64f0f3371f5db3c0302ad5303a61f01830d327
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-08-28 12:09:25 +0530

    Time: 13 ms (26.42%), Space: 8.7 MB (82.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,21 +3,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-//         std::sort(s.begin(), s.end());
-//     std::sort(t.begin(), t.end());
+        std::sort(s.begin(), s.end());
+    std::sort(t.begin(), t.end());
         
-//     return s==t;
+    return s==t;
     
-    if (s.size() != t.size()) {
-            return false;
-        }
-        unordered_map<char, int> sMap, tMap;
-        for (char c : s) {
-            sMap[c]++;
-        }
-        for (char c : t) {
-            tMap[c]++;
-        }
-        return sMap == tMap;
+    // if (s.size() != t.size()) {
+    //         return false;
+    //     }
+    //     unordered_map<char, int> sMap, tMap;
+    //     for (char c : s) {
+    //         sMap[c]++;
+    //     }
+    //     for (char c : t) {
+    //         tMap[c]++;
+    //     }
+    //     return sMap == tMap;
     }
 };
