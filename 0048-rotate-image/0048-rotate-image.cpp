ca956ef2aceb464866739bcc56997d634cf57345
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-09-02 11:59:08 +0530

    Time: 0 ms (100.00%), Space: 9 MB (52.37%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,7 @@ public:
         int n = matrix.size();
         cout << n<< " ";
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n-1; i++) {
         for (int j = i + 1; j < n; j++) {
             swap(matrix[i][j], matrix[j][i]);
         }
