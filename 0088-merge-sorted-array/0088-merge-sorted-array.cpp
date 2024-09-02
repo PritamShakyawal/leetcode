74cbf38c01e05243488bf5452c3da0315d6c267d
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-09-02 12:42:15 +0530

    Time: 5 ms (28.58%), Space: 11.1 MB (89.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i=0, j=0;
-        vector<int> nums3;
-        while(i<m && j<n){
-            if(nums1[i]<=nums2[j]){
-                nums3.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                nums3.push_back(nums2[j]);
-                j++;
-            }
+//         int i=0, j=0;
+//         vector<int> nums3;
+//         while(i<m && j<n){
+//             if(nums1[i]<=nums2[j]){
+//                 nums3.push_back(nums1[i]);
+//                 i++;
+//             }
+//             else{
+//                 nums3.push_back(nums2[j]);
+//                 j++;
+//             }
             
-        }
+//         }
         
-        while(i<m){
-            nums3.push_back(nums1[i]);
-            i++;
-        }
+//         while(i<m){
+//             nums3.push_back(nums1[i]);
+//             i++;
+//         }
 
-        while(j<n){
-            nums3.push_back(nums2[j]);
-            j++;
-        }
-        // cout << nums1.size();
-
-        nums1 = nums3;
+//         while(j<n){
+//             nums3.push_back(nums2[j]);
+//             j++;
+//         }
+//         // cout << nums1.size();
+//            nums1 = nums3;
         
+      
+        
+       int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+        while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+
+    }
     }
 };
