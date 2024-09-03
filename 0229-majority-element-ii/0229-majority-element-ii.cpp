0807065f64a8504e1500a140b979ccf71a2ab34d
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-09-03 11:13:27 +0530

    Time: 12 ms (29.33%), Space: 18.8 MB (36.64%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size(); 
+    vector<int> ls; 
+    map<int, int> mpp;
+    
+    for (int i = 0; i < n; i++) {
+        mpp[nums[i]]++;
+    }
+       
+       for(auto it : mpp){
+        if(it.second > n/3){
+            ls.push_back(it.first);
+        }
+       }
+    return ls;
+    }
+
+
+};
