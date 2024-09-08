c428f0251ae3ad5b718cbf99939c52dcc86248bd
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-09-08 15:05:44 +0530

    Time: 6 ms (36.31%), Space: 15.3 MB (29.77%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,6 +12,36 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+//         vector<vector<int>> result;
+//         if(root == NULL) return result;
+        
+//         queue<TreeNode*> nodes;
+//         nodes.push(root);
+//         bool leftToRight = true;
+        
+//         while(!nodes.empty()){
+//             int size = nodes.size();
+//             vector<int> ans(size);
+            
+            
+//             for(int i=0; i<size; i++){
+//                 TreeNode* node = nodes.front();
+//                 nodes.pop();
+                
+//                 int index = (leftToRight) ? i : size-i-1;
+                
+//                 ans[index] = node->val;
+                
+//                 if(node->left) nodes.push(node->left);
+//                 if(node->right) nodes.push(node->right);
+//             }
+//             result.push_back(ans);
+//         }
+//         return result;
+        
+        
+        
+        
         vector<vector<int>> result;
         if(root == NULL) return result;
         
@@ -21,16 +51,15 @@ public:
         
         while(!nodes.empty()){
             int size = nodes.size();
-            vector<int> ans(size);
+            vector<int> ans;
             
             
             for(int i=0; i<size; i++){
                 TreeNode* node = nodes.front();
                 nodes.pop();
                 
-                int index = (leftToRight) ? i : size-i-1;
-                
-                ans[index] = node->val;
+                // ans[index] = node->val;
+                ans.push_back(node->val);
                 
                 if(node->left) nodes.push(node->left);
                 if(node->right) nodes.push(node->right);
