0e67169f9737e635112855b16b867cf3f68e7168
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-10-30 22:36:41 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (75.36%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -8,14 +8,24 @@ public:
 //         return ans;
         
         
-        if (n <= 1)
-            return n;
-        vector<int> dp(n + 1, -1);
+//         if (n <= 1)
+//             return n;
+//         vector<int> dp(n+1, -1);
 
-        if (dp[n] != -1)
-            return dp[n];
+//         if (dp[n] != -1)
+//             return dp[n];
 
-        dp[n] = fib(n - 1) + fib(n - 2);
+//         dp[n] = fib(n - 1) + fib(n - 2);
+//         return dp[n];
+        
+        if (n == 0) return 0;
+        vector<int> dp(n+1, -1);
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
-    }
+    } 
 };
