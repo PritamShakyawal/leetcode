435d34b0ca1e8fe2d231e20350acb90b70030369
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-11-12 00:42:45 +0530

    Time: 0 ms (100.00%), Space: 14 MB (49.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,16 +2,16 @@ class Solution {
     
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    map<int, int> m;
-    for(int i=0; i<nums.size(); i++){
-        int a = nums[i];
-        int remValue = target - a;
-        if(m.find(remValue) != m.end()){
-            return {m[remValue], i};
-        }
-            m[a] = i;    
-    }
-     return {-1, -1};
+    // map<int, int> m;
+    // for(int i=0; i<nums.size(); i++){
+    //     int a = nums[i];
+    //     int remValue = target - a;
+    //     if(m.find(remValue) != m.end()){
+    //         return {m[remValue], i};
+    //     }
+    //         m[a] = i;    
+    // }
+    //  return {-1, -1};
         
     // vector<int> ans;
     // int n = nums.size();
@@ -25,6 +25,38 @@ public:
     //     }
     // }
     // return { -1, -1};
+        
+        
+       vector<pair<int, int>> numWithIndex;
+    int n = nums.size();
+    vector<int> ans;
+
+    // Store values with their original indices
+    for (int i = 0; i < n; i++) {
+        numWithIndex.push_back({nums[i], i});
+    }
+
+    // Sort based on the values
+    sort(numWithIndex.begin(), numWithIndex.end());
+
+    int l = 0, r = n - 1;
+
+    while (l < r) {
+        int value = numWithIndex[l].first + numWithIndex[r].first;
+
+        if (value < target) {
+            l++;
+        } else if (value > target) {
+            r--;
+        } else {
+            // Return the original indices
+            ans.push_back(numWithIndex[l].second);
+            ans.push_back(numWithIndex[r].second);
+            return ans;
+        }
+    }
+
+    return {-1, -1};
         
     }
 };
