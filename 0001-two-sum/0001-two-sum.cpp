65f5d4350f1f2b6562d4828c4f3c0d0e20f09156
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-11-12 00:26:41 +0530

    Time: 5 ms (56.07%), Space: 14.6 MB (17.03%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,29 +2,29 @@ class Solution {
     
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    // map<int, int> m;
-    // for(int i=0; i<nums.size(); i++){
-    //     int a = nums[i];
-    //     int remValue = target - a;
-    //     if(m.find(remValue) != m.end()){
-    //         return {m[remValue], i};
-    //     }
-    //         m[a] = i;    
-    // }
-    //  return {-1, -1};
-        
-        vector<int> ans;
-        int n = nums.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (nums[i] + nums[j] == target) {
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
+    map<int, int> m;
+    for(int i=0; i<nums.size(); i++){
+        int a = nums[i];
+        int remValue = target - a;
+        if(m.find(remValue) != m.end()){
+            return {m[remValue], i};
         }
+            m[a] = i;    
     }
-    return { -1, -1};
+     return {-1, -1};
+        
+    // vector<int> ans;
+    // int n = nums.size();
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = i + 1; j < n; j++) {
+    //         if (nums[i] + nums[j] == target) {
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             return ans;
+    //         }
+    //     }
+    // }
+    // return { -1, -1};
         
     }
 };
