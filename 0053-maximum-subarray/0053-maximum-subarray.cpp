ed76048307dff5b3354472f9a647e6a42ff4a210
Pritam <95740918+PritamShakyawal@users.noreply.github.com>
2024-11-20 23:28:36 +0530

    Time: 0 ms (100.00%), Space: 70.6 MB (23.60%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -20,5 +20,15 @@ public:
         }
         
         return maxi;
+        
+        
+        // for(int i=0; i<n; i++){
+        //     int sum = 0;
+        //     for(int j=i; j<n; j++){
+        //         sum += nums[j];
+        //         maxi = max(maxi, sum);
+        //     }
+        // }
+        // return maxi;
     }
 };
